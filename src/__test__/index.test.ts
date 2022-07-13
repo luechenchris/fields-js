dfe31bfcfb07d5257a155f89d9de7f0781151d9b
Luechen Christopher <luechenchris@gmail.com>
2022-07-13 10:26:54 -0500

    feat: added form group tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,25 +1,84 @@
 import Form from '../index';
 
-test('Valid Email', () => {
-  let form = new Form({
-    email: {
-      value: null,
-      validator: ['email'],
-    }
-  });
-  form.update('email', 'johndoe@gmail.com');
-  let { valid } = form.value();
-  expect(valid).toBe(true);
+let form = new Form({
+  email: {
+    value: null,
+    validator: ['email'],
+  }
 });
 
-test('Invalid Email', () => {
-  let form = new Form({
-    email: {
-      value: null,
-      validator: ['email'],
-    }
+describe('Form Validation', () => {
+  test('Valid Email', () => {
+    form.update('email', 'johndoe@gmail.com');
+    let { valid } = form.value();
+    expect(valid).toBe(true);
   });
-  form.update('email', 'johndoegmail.com');
-  let { valid } = form.value();
-  expect(valid).toBe(false);
+
+  test('Invalid Email', () => {
+    form.update('email', 'johndoegmail.com');
+    let { valid, form: fields } = form.value();
+    expect(valid).toBe(false);
+    expect(fields.email.value).toBe('johndoegmail.com');
+  });
+
+  test('Form Reset', () => {
+    form.resetAll();
+    let { valid, form: fields } = form.value();
+    expect(valid).toBe(false);
+    expect(fields.email.value).toBe('');
+  });
+});
+
+describe('Adding Fields To Form', () => {
+  test('Add Form Group', () => {
+    form.addField('users', [{
+      name: {
+        value: null,
+        validator: ['required'],
+      },
+      email: {
+        value: null,
+        validator: ['email'],
+      }
+    }]);
+    let { form: fields } = form.value();
+    expect(fields.users[0].name.value).toBeNull();
+  });
+
+  test('Add Field To Form Group', () => {
+    form.addField('users', {
+      name: {
+        value: null,
+        validator: ['required'],
+      },
+      email: {
+        value: 'invalid email',
+        validator: ['email'],
+      }
+    });
+    let { form: fields } = form.value();
+    expect(fields.users[1].email.value).toBe('invalid email');
+  });
+});
+
+describe('Update Form Group', () => {
+  beforeAll(() => {
+    // Remove email field
+    form.removeField('email');
+  });
+
+  test('Update Form Group', () => {
+    form.update('users', Form.group(0, { name: 'John Doe', email: 'john@mail.com' }));
+    form.update('users', Form.group(1, { name: 'Jane', email: 'jane@mail.com' }));
+    let { form: fields } = form.value();
+    expect(fields.users[0].name.value).toBe('John Doe');
+    expect(fields.users[0].email.value).toBe('john@mail.com');
+    expect(fields.users[1].name.value).toBe('Jane');
+    expect(fields.users[1].email.value).toBe('jane@mail.com');
+  });
+
+  test('Validate Form', () => {
+    let { valid } = form.value();
+    expect(valid).toBe(true);
+  });
 });
